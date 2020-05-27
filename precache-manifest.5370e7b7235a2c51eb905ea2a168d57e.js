ab4777628877086bdfcb0b9b2348b4984cb8cb10
David Barenco <david.barenco@neoprospecta.com>
2020-05-27 08:50:19 -0300

    Updates

---
+++ precache-manifest.5370e7b7235a2c51eb905ea2a168d57e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effbcec21a1d3438b25e6328742ecc8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9022612acae2b460fee8",
+    "url": "/static/css/main.5cf7e161.chunk.css"
+  },
+  {
+    "revision": "57ae05a77834d59e99bb",
+    "url": "/static/js/2.d517eb51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d517eb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9022612acae2b460fee8",
+    "url": "/static/js/main.1e478e98.chunk.js"
+  },
+  {
+    "revision": "2dc9d719c56063e714a4",
+    "url": "/static/js/runtime-main.edf8ef9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
